8dc05414e7e985a7c67bfeb2c9e5a038d84dac19
conradgoh1990 <conrad.goh.1990@gmail.com>
2023-08-25 18:33:45 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 // import pages
+import Socials from './Socials';
+import Logo from '../img/header/logo.svg';
+import MobileNav from './MobileNav';
+
+// import Link
+import { Link} from 'react-router-dom';
 
 
 const Header = () => {
-  return <div>Header</div>;
+  return(
+      <header className='bg-pink-200 fixed w-full px-[30px]
+      lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center
+      '>
+        Header
+      </header>
+  );
 };
 
 export default Header;
